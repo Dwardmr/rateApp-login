89524766ee2a4ce8123f42f9252ec804e026f335
Eduardo Martinez Ruiz <dwardmr@gmail.com>
2016-09-12 13:38:21 -0600

    Buttons overhauling

--- src/js/Components/fbsigninButton.js
+++ src/js/Components/fbsigninButton.js
@@ -4,7 +4,7 @@ export default class FbSignInButton extends React.Component{
 	render(){
 		return(
 			<div class="col l12 m12 s12">
-				<a class="waves-effect waves-light btn light-blue darken-1"><i class="material-icons right">send</i>SIGN IN WITH FACEBOOK</a>
+				<a class="waves-effect waves-light btn fbLoginButton"><i class="material-icons left">send</i>SIGN IN WITH FACEBOOK</a>
 			</div>
 		);
 	}
--- src/js/Components/userInput.js
+++ src/js/Components/userInput.js
@@ -7,7 +7,7 @@ export default class UserInput extends React.Component{
 	render(){
 		return(
 			<div class="UserInput">
-				<div class="row center-align">
+				<div class="row">
 					<SignUpButton />					
 				</div>
 				<div class="row center-align">
@@ -15,7 +15,7 @@ export default class UserInput extends React.Component{
 				</div>
 					<div class="row center-align">
 						<div class="col l12 m12 s12">
-							<p class="signIn-Text">Already have an account? Please <a href="#!">Sign In </a></p>
+							<p class="signIn-Text">Already have an account? Please <a href="#!" class="signInButton">Sign In</a></p>
 						</div>
 					</div>
 			</div>
