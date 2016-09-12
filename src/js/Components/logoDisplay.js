70c151e368ad16f75b796e00910ebd084a45fb07
Eduardo Martinez Ruiz <dwardmr@gmail.com>
2016-09-12 11:03:29 -0600

    Added logo

--- src/js/Components/logoDisplay.js
+++ src/js/Components/logoDisplay.js
@@ -5,6 +5,7 @@ export default class LogoDisplay extends React.Component{
 		return(
 				<div class="col l12 m12 s12">
 					<div class="square">
+						<img src="../../img/logo.png" class="logoCont"></img>
 					</div>
 				</div>
 		);
